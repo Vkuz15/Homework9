be032e2658c567280b90484ee287ebd37ab17476
Владимир <vkuznecov1504@mail.ru>
2024-01-27 20:09:28 +0400

    Вывели в консоль с помощью цикла for максимальную и минимальную сумму за месяц

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         task1();
+        task2();
     }
 
     public static int[] generateRandomArray() {
@@ -19,5 +20,24 @@ public class Main {
             sum += arr[index];
         }
         System.out.println("Сумма трат за месяц составила " + sum + " рублей.");
+    }
+
+    public static void task2() {
+        System.out.println("Задача 2");
+        int[] arr = generateRandomArray();
+        int maxSum = -1;
+        int minSum = arr[0] + 1;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > maxSum) {
+                maxSum = arr[i];
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < minSum) {
+                minSum = arr[i];
+            }
+        }
+        System.out.println("Максимальная сумма трат за день составила " + maxSum + " рублей.");
+        System.out.println("Минимальная сумма трат за день составила " + minSum + " рублей.");
     }
 }
